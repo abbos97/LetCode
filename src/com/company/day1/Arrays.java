478d8b3bfde9a9d81a6bc6916fc7c10e403f504d
abbos97 <58409711+abbos97@users.noreply.github.com>
2020-04-18 23:19:12 -0400

    Update Arrays.java

--- src/com/company/day1/Arrays.java
+++ src/com/company/day1/Arrays.java
@@ -3,5 +3,8 @@ package com.company.day1;
 public class Arrays {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+         System.out.println("My name is Abbos Shirinboev");
+        
+        int num= 21;
     }
 }
